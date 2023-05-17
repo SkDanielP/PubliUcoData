0260fe62d46e5f9ab821a2d53c39a3b3b8795d26
SkDanielP <113185316+SkDanielP@users.noreply.github.com>
2023-05-17 17:30:04 -0500

    actualizacion data

--- PubliUcoData/src/co/edu/uco/publiuco/data/dao/factory/DAOFactory.java
+++ PubliUcoData/src/co/edu/uco/publiuco/data/dao/factory/DAOFactory.java
@@ -1,7 +1,32 @@
 package co.edu.uco.publiuco.data.dao.factory;
 
+import co.edu.uco.publiuco.data.dao.AdministradorCategoriaDAO;
+import co.edu.uco.publiuco.data.dao.CalificacionDAO;
+import co.edu.uco.publiuco.data.dao.CategoriaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoAdministradorCategoriaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoCategoriaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoComentarioLectorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoEscritorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoLectorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoObservacionRevisorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoPlanDAO;
+import co.edu.uco.publiuco.data.dao.EstadoPreferenciaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoPublicacionDAO;
+import co.edu.uco.publiuco.data.dao.EstadoRevisionDAO;
+import co.edu.uco.publiuco.data.dao.EstadoRevisorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoSuscripcionDAO;
+import co.edu.uco.publiuco.data.dao.EstadoTipoAccesoDAO;
+import co.edu.uco.publiuco.data.dao.EstadoTipoEscritorDAO;
 import co.edu.uco.publiuco.data.dao.EstadoTipoRelacionInstitucionDAO;
+import co.edu.uco.publiuco.data.dao.LectorDAO;
+import co.edu.uco.publiuco.data.dao.PublicacionDAO;
+import co.edu.uco.publiuco.data.dao.TipoAccesoDAO;
+import co.edu.uco.publiuco.data.dao.TipoComentarioRevisorDAO;
+import co.edu.uco.publiuco.data.dao.TipoEscritorDAO;
+import co.edu.uco.publiuco.data.dao.TipoIdentificacionDAO;
 import co.edu.uco.publiuco.data.dao.TipoRelacionInstitucionDAO;
+import co.edu.uco.publiuco.data.dao.TipoReporteDAO;
+import co.edu.uco.publiuco.data.dao.TipoRevisionDAO;
 import co.edu.uco.publiuco.data.dao.factory.relational.sqlserver.SqlServerDAOFactory;
 import co.edu.uco.publiuco.data.dao.relational.postgresql.PostgreSqlDAOFactory;
 
@@ -41,5 +66,55 @@ public abstract class DAOFactory {
 	public abstract EstadoTipoRelacionInstitucionDAO getEstadoTipoRelacionInstitucionDAO();
 	
 	public abstract TipoRelacionInstitucionDAO getTipoRelacionInstitucionDAO();
+
+	public abstract AdministradorCategoriaDAO getAdmniAdministradorCategoriaDAO();
+	
+	public abstract CalificacionDAO getCalificacionDAO();
+	
+	public abstract CategoriaDAO getCategoriaDAO();
+	
+	public abstract EstadoAdministradorCategoriaDAO getAdministradorCategoriaDAO();
+	
+	public abstract EstadoCategoriaDAO getEstadoCategoriaDAO();
+	
+	public abstract EstadoComentarioLectorDAO getEstadoComentarioLectorDAO();
+	
+	public abstract EstadoEscritorDAO getEstadoEscritorDAO();
+	
+	public abstract EstadoLectorDAO getEstadoLectorDAO();
+	
+	public abstract EstadoObservacionRevisorDAO getEstadoObservacionRevisorDAO();
+	
+	public abstract EstadoPlanDAO getEstadoPlanDAO();
+	
+	public abstract EstadoPreferenciaDAO getEstadoPreferenciaDAO();
+	
+	public abstract EstadoPublicacionDAO estadoPublicacionDAO();
+	
+	public abstract EstadoRevisionDAO getEstadoRevisionDAO();
+	
+	public abstract EstadoRevisorDAO getEstadoRevisorDAO();
+	
+	public abstract EstadoSuscripcionDAO getEstadoSuscripcionDAO();
+	
+	public abstract EstadoTipoAccesoDAO getEstadoTipoAccesoDAO();
+	
+	public abstract EstadoTipoEscritorDAO getEstadoTipoEscritorDAO();
+	
+	public abstract LectorDAO getLectorDAO();
+	
+	public abstract PublicacionDAO getEstadoPublicacionDAO();
+	
+	public abstract TipoAccesoDAO getTipoAccesoDAO();
+	
+	public abstract TipoComentarioRevisorDAO getTipoComentarioRevisorDAO();
+	
+	public abstract TipoEscritorDAO getTipoEscritorDAO();
+	
+	public abstract TipoIdentificacionDAO getTipoIdentificacionDAO();
+	
+	public abstract TipoReporteDAO getTipoReporteDAO();
+	
+	public abstract TipoRevisionDAO getTipoRevisionDAO();
 
 }
--- PubliUcoData/src/co/edu/uco/publiuco/data/dao/factory/relational/sqlserver/SqlServerDAOFactory.java
+++ PubliUcoData/src/co/edu/uco/publiuco/data/dao/factory/relational/sqlserver/SqlServerDAOFactory.java
@@ -2,8 +2,33 @@ package co.edu.uco.publiuco.data.dao.factory.relational.sqlserver;
 
 import java.sql.Connection;
 
+import co.edu.uco.publiuco.data.dao.AdministradorCategoriaDAO;
+import co.edu.uco.publiuco.data.dao.CalificacionDAO;
+import co.edu.uco.publiuco.data.dao.CategoriaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoAdministradorCategoriaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoCategoriaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoComentarioLectorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoEscritorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoLectorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoObservacionRevisorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoPlanDAO;
+import co.edu.uco.publiuco.data.dao.EstadoPreferenciaDAO;
+import co.edu.uco.publiuco.data.dao.EstadoPublicacionDAO;
+import co.edu.uco.publiuco.data.dao.EstadoRevisionDAO;
+import co.edu.uco.publiuco.data.dao.EstadoRevisorDAO;
+import co.edu.uco.publiuco.data.dao.EstadoSuscripcionDAO;
+import co.edu.uco.publiuco.data.dao.EstadoTipoAccesoDAO;
+import co.edu.uco.publiuco.data.dao.EstadoTipoEscritorDAO;
 import co.edu.uco.publiuco.data.dao.EstadoTipoRelacionInstitucionDAO;
+import co.edu.uco.publiuco.data.dao.LectorDAO;
+import co.edu.uco.publiuco.data.dao.PublicacionDAO;
+import co.edu.uco.publiuco.data.dao.TipoAccesoDAO;
+import co.edu.uco.publiuco.data.dao.TipoComentarioRevisorDAO;
+import co.edu.uco.publiuco.data.dao.TipoEscritorDAO;
+import co.edu.uco.publiuco.data.dao.TipoIdentificacionDAO;
 import co.edu.uco.publiuco.data.dao.TipoRelacionInstitucionDAO;
+import co.edu.uco.publiuco.data.dao.TipoReporteDAO;
+import co.edu.uco.publiuco.data.dao.TipoRevisionDAO;
 import co.edu.uco.publiuco.data.dao.factory.DAOFactory;
 import co.edu.uco.publiuco.data.dao.relational.sqlserver.EstadoTipoRelacionInstitucionSqlServerDAO;
 import co.edu.uco.publiuco.data.dao.relational.sqlserver.TipoRelacionInstitucionSqlServerDAO;
@@ -55,6 +80,156 @@ public final class SqlServerDAOFactory extends DAOFactory {
 	@Override
 	public TipoRelacionInstitucionDAO getTipoRelacionInstitucionDAO() {
 		return new TipoRelacionInstitucionSqlServerDAO(connection);
+	}
+
+	@Override
+	public AdministradorCategoriaDAO getAdmniAdministradorCategoriaDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CalificacionDAO getCalificacionDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoriaDAO getCategoriaDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoAdministradorCategoriaDAO getAdministradorCategoriaDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoCategoriaDAO getEstadoCategoriaDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoComentarioLectorDAO getEstadoComentarioLectorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoEscritorDAO getEstadoEscritorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoLectorDAO getEstadoLectorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoObservacionRevisorDAO getEstadoObservacionRevisorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoPlanDAO getEstadoPlanDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoPreferenciaDAO getEstadoPreferenciaDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoPublicacionDAO estadoPublicacionDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoRevisionDAO getEstadoRevisionDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoRevisorDAO getEstadoRevisorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoSuscripcionDAO getEstadoSuscripcionDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoTipoAccesoDAO getEstadoTipoAccesoDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstadoTipoEscritorDAO getEstadoTipoEscritorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LectorDAO getLectorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PublicacionDAO getEstadoPublicacionDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoAccesoDAO getTipoAccesoDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoComentarioRevisorDAO getTipoComentarioRevisorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoEscritorDAO getTipoEscritorDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoIdentificacionDAO getTipoIdentificacionDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoReporteDAO getTipoReporteDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoRevisionDAO getTipoRevisionDAO() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
